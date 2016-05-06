3345b339cc66f9ea02fce5319aa2df4da79a7c1d
Koos Kleven <koos42@gmail.com>
2016-05-06 15:05:10 -0700

    compare test.conn resp_body with that from curl

--- test/lib/chunking_issue_example/plug_test.exs
+++ test/lib/chunking_issue_example/plug_test.exs
@@ -6,12 +6,15 @@ defmodule ChunkingIssueExample.PlugTest do
 
   doctest ChunkingIssueExample.Plug
 
-  test "sending a chunked response works" do
+  test "sending a chunked response works and it matches CURL" do
     conn = conn(:get, "/")
     conn = ChunkingIssueExample.Plug.call(conn, @opts)
+    {curl_resp_body, 0} = IO.inspect System.cmd("curl", ["-s", "localhost:4000"])
 
     assert conn.state == :chunked
     assert conn.status == 200
     assert conn.resp_body == "hello\nworld\ngoodbye\n"
+
+    assert conn.resp_body == curl_resp_body
   end
 end
