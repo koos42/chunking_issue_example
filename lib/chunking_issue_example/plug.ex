d0de6cd15fc398d98151b3601d24248a449330d6
Koos Kleven <koos42@gmail.com>
2016-05-06 15:53:43 -0700

    make the example ensure that the connection is still open.

--- lib/chunking_issue_example/plug.ex
+++ lib/chunking_issue_example/plug.ex
@@ -8,10 +8,10 @@ defmodule ChunkingIssueExample.Plug do
   def call(conn, _opts) do
     conn = put_resp_content_type(conn, "text/plain")
     conn = send_chunked(conn, 200)
-    {:ok, conn} = chunk(conn, "hello\n")
-    {:ok, conn} = chunk(conn, "world\n")
-    {:ok, conn} = chunk(conn, "")
-    {:ok, conn} = chunk(conn, "goodbye\n")
+    {:ok, %Plug.Conn{state: :chunked} = conn} = chunk(conn, "hello\n")
+    {:ok, %Plug.Conn{state: :chunked} = conn} = chunk(conn, "world\n")
+    {:ok, %Plug.Conn{state: :chunked} = conn} = chunk(conn, "")
+    {:ok, %Plug.Conn{state: :chunked} = conn} = chunk(conn, "goodbye\n")
     conn
   end
 end
